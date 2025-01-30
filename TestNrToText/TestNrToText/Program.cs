04e0e1239cdadd0bc9c5e60a94a1f76dd0700427
iwangoll <43299655+iwangoll@users.noreply.github.com>
2025-01-30 19:04:24 +0100

    Add files via upload

---
+++ TestNrToText/TestNrToText/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NrToText
+{
+    enum EnumTest
+    {
+        Von1bis100,
+        /// <summary>
+        /// für das Erzeugen von Serienbriefen im Excel:
+        /// man ermittelt die aktuellen Beträge, übergibt sie durch Komma getrennt an InTextZeilen. 
+        /// Das Ergebnis speichert man in einer extra Tabelle der Mappe
+        /// und fügt (z.B. in der Tabelle für den Serienbrief) mittels SVERWEIS die Bezeichnung 
+        /// für die Zahl in der Spalte "Betrag" in die Spalte "BetragText" ein.
+        /// </summary>
+        SpezielleZahlenFuerExcel_SVERWEIS, 
+        /// <summary>
+        /// klappt es auch für große Zahlen
+        /// </summary>
+        GrosseZahlen,
+        /// <summary>
+        /// im Original 37 -> siebenunddreizig
+        /// </summary>
+        Dreissig,
+        /// <summary>
+        /// im Original 100 -> einhundertnul
+        /// </summary>
+        HundertRund  
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WasTun(EnumTest.SpezielleZahlenFuerExcel_SVERWEIS);
+            //Console.ReadKey();
+        }
+
+        private static void WasTun(EnumTest wasTun)
+        {
+            string name = wasTun.ToString();
+            Debug.WriteLine($"{name}:\n{new string('=', name.Length+1)}");
+            switch (wasTun)
+            {
+                case EnumTest.Von1bis100:
+                    for (int i = 1; i < 100; i++)
+                    {
+                        Debug.WriteLine(InText(i));
+                    }
+                    break;
+                case EnumTest.SpezielleZahlenFuerExcel_SVERWEIS:
+                    Debug.WriteLine(InTextZeilen(10, 12, 15, 20, 24, 25, 30, 35, 40, 45, 50, 57, 60, 65, 69, 70, 75, 100, 130, 144, 150, 165, 180, 200, 300, 311, 500));
+                    break;
+                case EnumTest.GrosseZahlen:
+                    Debug.WriteLine(InTextZeilen(101215, 2024253035));
+                    break;
+                case EnumTest.Dreissig:
+                    Debug.WriteLine(InText(37));
+                    break;
+                case EnumTest.HundertRund:
+                    Debug.WriteLine(InText(100));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static string InTextZeilen(params int[] arrayV)
+        {
+            List<string> list = arrayV.ToList().Select(x => InText(x)).ToList();
+            return String.Join("\r\n", list);
+        }
+        private static string InText(int v)
+        {
+            return $"{v}\t{new WandleZahlInText(v).ToString()}";
+        }
+    }
+}
---
+++ TestNrToText/TestNrToText/WandleZahlInText.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NrToText
+{
+    /// <summary>
+    /// Converts number in text
+    /// </summary>
+    public class WandleZahlInText
+    {
+        private long m_nr;
+        private enum NrTypes { Count, Feminin, Masc };
+        private NrTypes nrt = NrTypes.Count;
+        public WandleZahlInText(long i)
+        {
+            m_nr = i;
+        }
+        public override string ToString()
+        {
+            const long milliard = (int)1e9;
+            const long million = (int)1e6;
+            const long tausend = (int)1e3;
+            if (m_nr < tausend)
+                return NullTo999();
+            string ret = "";
+            int md = (int)(m_nr / milliard);
+            if (md != 0)
+            {
+                WandleZahlInText c = new WandleZahlInText(md);
+                c.nrt = NrTypes.Feminin;
+                ret = ret + c.NullTo999(); // to throw an exception if too big!
+                if (md == 1)
+                    ret = ret + "milliarde";
+                else
+                    ret = ret + "milliarden"; // echt milliarder :)
+            }
+            int mn = (int)((m_nr - md * milliard) / million);
+            if (mn != 0)
+            {
+                WandleZahlInText v = new WandleZahlInText(mn);
+                v.nrt = NrTypes.Feminin;
+                ret = ret + v.NullTo999();
+                if (mn == 1)
+                    ret = ret + "millione";
+                else
+                    ret = ret + "millionen";
+            }
+            int tz = (int)((m_nr - md * milliard - mn * million) / tausend);
+            if (tz != 0)
+            {
+                WandleZahlInText v = new WandleZahlInText(tz);
+                v.nrt = NrTypes.Masc;  // eintausend !!
+                ret = ret + v.NullTo999() + "tausend";
+            }
+            int einheit = (int)(m_nr - md * milliard - mn * million - tz * tausend);
+            if (einheit != 0)
+            {
+                WandleZahlInText b = new WandleZahlInText(einheit);
+                ret = ret + b.ToString();
+            }
+            return ret;
+        }
+
+        private string NullTo999()
+        {
+            if (m_nr > 999)
+                throw new Exception();
+            if (m_nr < 100)
+                return NullTo99();
+            int hund = (int)(m_nr / 100);
+            string ret = "";
+            if (hund != 0)
+            {
+                WandleZahlInText c = new WandleZahlInText(hund);
+                c.nrt = NrTypes.Masc; // neuter ist gleich
+                ret = c.ToString() + "hundert";
+            }
+
+            // im Original WandleZahlInText c1 = new WandleZahlInText(m_nr % 100);
+            long modulo100 = m_nr % 100;
+            string textModulo100 = modulo100 == 0 ? "" : new WandleZahlInText(modulo100).ToString();
+            ret = ret + textModulo100;
+            return ret;
+        }
+
+        private string NullTo99()
+        {
+            if (m_nr > 99)
+                throw new Exception();
+            if (m_nr < 13)
+                return NullTo12();
+            if (13 <= m_nr && m_nr < 20)
+            {
+                WandleZahlInText c = new WandleZahlInText(m_nr - 10);
+                return c.GetNrCompForm() + "zehn";
+
+            }
+
+            // bigger then 20
+            int deci = (int)m_nr / 10;
+            int unit = (int)m_nr % 10;
+            string ret = "";
+            if (unit != 0)
+            {
+                WandleZahlInText c = new WandleZahlInText(unit);
+                ret = c.ToString() + "und";
+            }
+            WandleZahlInText x = new WandleZahlInText(deci);
+            // im Original ret = ret + x.GetNrCompForm() + "zig";
+            ret = ret + x.GetNrCompForm() + (x.m_nr == 3 ? "ßig" : "zig");
+            return ret;
+        }
+
+        private string GetNrCompForm()
+        {
+            if (m_nr > 9)
+                throw new Exception();
+            string r = NullTo12();
+            if (m_nr == 2)
+                return "zwan"; // für zwanzig
+            if (m_nr == 6)
+                return "sech"; // für siebzig und siebzehn
+            if (m_nr == 7)
+                return "sieb"; // für siebzig und siebzehn
+            return r; // correct number
+        }
+
+
+
+
+
+        private string NullTo12()
+        {
+            switch (m_nr)
+            {
+                case 0:
+                    return "nul";
+                case 1:
+                    {
+                        return GetEins();
+                    }
+                case 2:
+                    return "zwei";
+                case 3:
+                    return "drei";
+                case 4:
+                    return "vier";
+                case 5:
+                    return "fünf";
+                case 6:
+                    return "sechs";
+                case 7:
+                    return "sieben";
+                case 8:
+                    return "acht";
+                case 9:
+                    return "neun";
+                case 10:
+                    return "zehn";
+                case 11:
+                    return "elf";
+                case 12:
+                    return "zwölf";
+                default:
+                    throw new Exception();
+            }
+        }
+
+        private string GetEins()
+        {
+            switch (nrt)
+            {
+                case NrTypes.Count:
+                    return "eins";
+                case NrTypes.Masc:
+                    return "ein";
+                case NrTypes.Feminin:
+                    return "eine";
+            }
+            return "";
+        }
+    }
+
+
+}
+
